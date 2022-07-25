f6f8dd26ead43fc9fd2479f6f25b60a7a1bf5859
SKurse26 <109313811+SKurse26@users.noreply.github.com>
2022-07-25 13:38:59 -0700

    Update streamlit_app.py
    Added emoji and new item

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,11 @@ import streamlit
 streamlit.title('My first code with stram')
 #Adding my code now here
 streamlit.title('My New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach, and Rocket Smoothie')
-streamlit.text('Hard boiled Free range egg')
+# Changing the header
+streamlit.header('Breakfast Favorites')
+#Adding emoji
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('  🥗 Kale, Spinach, and Rocket Smoothie')
+streamlit.text(' 🐔 Hard boiled Free range egg')
+#adding new item
+streamlit.text(' 🥑🍞 Avocado Toast') 
